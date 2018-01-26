9cff087876f0a4f9386c97a3597dfaf9f5b54048
Robin Radic <robin@radic.nl>
2018-01-26 18:50:41 +0100

    upudat

--- src/Native/AbstractTestCase.php
+++ src/Native/AbstractTestCase.php
@@ -1,15 +1,20 @@
 <?php
 /**
- * Part of the Laradic PHP packages.
+ * Part of the Laradic PHP Packages.
  *
- * License and copyright information bundled with this package in the LICENSE file
+ * Copyright (c) 2018. Robin Radic.
+ *
+ * The license can be found in the package and online at https://laradic.mit-license.org.
+ *
+ * @copyright Copyright 2018 (c) Robin Radic
+ * @license https://laradic.mit-license.org The MIT License
  */
 namespace Laradic\Testing\Native;
 
 
 use Laradic\Testing\Native\Traits\PHPUnitTrait;
 
-abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
+abstract class AbstractTestCase extends \PHPUnit\Framework\TestCase
 {
     use PHPUnitTrait;
 
