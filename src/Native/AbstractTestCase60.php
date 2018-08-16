b0cf609e70354726f833fe5fc8be290f1bc0b357
Robin Radic <robin@radic.nl>
2018-08-16 02:22:41 +0200

    add phpunit version

---
+++ src/Native/AbstractTestCase60.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Part of the Laradic PHP packages.
+ *
+ * License and copyright information bundled with this package in the LICENSE file
+ */
+namespace Laradic\Testing\Native;
+
+
+use Laradic\Testing\Native\Traits\PHPUnitTrait;
+
+abstract class AbstractTestCase extends \PHPUnit\Framework\TestCase
+{
+    use PHPUnitTrait;
+
+    public function testTest()
+    {
+        static::assertTrue(true);
+    }
+}
